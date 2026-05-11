42f0159107be650e9324574896fd2fe50d9775a2
Yurijfedor <yurijfedor178@gmail.com>
2026-05-11 19:11:29 +0300

    register tasks localization namespace

--- src/localization/i18n.ts
+++ src/localization/i18n.ts
@@ -1,27 +1,27 @@
 import i18n from 'i18next';
 
 import {initReactI18next} from 'react-i18next';
-
 import {DEFAULT_LANGUAGE} from './constants';
-
 import {getDeviceLanguage} from './helpers/getDeviceLanguage';
-
 import {loadLanguage, saveLanguage} from './storage';
 
 // EN
 import enCommon from './locales/en/common';
 import enNavigation from './locales/en/navigation';
 import enProfile from './locales/en/profile';
+import enTasks from './locales/en/tasks';
 
 // UK
 import ukCommon from './locales/uk/common';
 import ukNavigation from './locales/uk/navigation';
 import ukProfile from './locales/uk/profile';
+import ukTasks from './locales/uk/tasks';
 
 // DE
 import deCommon from './locales/de/common';
 import deNavigation from './locales/de/navigation';
 import deProfile from './locales/de/profile';
+import deTasks from './locales/de/tasks';
 
 import {AppLanguage} from './types';
 
@@ -77,7 +77,7 @@ export async function initLocalization() {
 
     defaultNS: 'common',
 
-    ns: ['common', 'navigation', 'profile'],
+    ns: ['common', 'navigation', 'profile', 'tasks'],
 
     interpolation: {
       escapeValue: false,
